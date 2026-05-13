ad41a45ac353328f3b3b9d470bcc9d9dcba5d994
Lucas Silva <lucascarlosjobs@gmail.com>
2026-05-12 22:19:28 -0300

    feat: adiciona rotas

--- frontend/ponto-certo-app/src/app/app.routes.ts
+++ frontend/ponto-certo-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './feature/inicio/inicio.component';
+import { FavoritosComponent } from './feature/favoritos/favoritos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'favoritos', component: FavoritosComponent },
+  { path: '**', redirectTo: '' }
+];
