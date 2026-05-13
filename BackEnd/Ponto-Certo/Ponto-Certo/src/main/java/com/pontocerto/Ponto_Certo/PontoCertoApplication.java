04408c1a1f54d4830a218e62b905bf2fcdd9072e
William <wllmsantiagogracioso@gmail.com>
2026-05-12 23:03:33 -0300

    Feat: Implementa e valida a proxy na api olho vivo da sptrans

--- BackEnd/Ponto-Certo/Ponto-Certo/src/main/java/com/pontocerto/Ponto_Certo/PontoCertoApplication.java
+++ BackEnd/Ponto-Certo/Ponto-Certo/src/main/java/com/pontocerto/Ponto_Certo/PontoCertoApplication.java
@@ -1,13 +1,37 @@
 package com.pontocerto.Ponto_Certo;
-
+import com.pontocerto.Ponto_Certo.service.ConsumoApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
+
 @SpringBootApplication
-public class PontoCertoApplication {
+public class PontoCertoApplication implements CommandLineRunner {
+	@Autowired
+	private ConsumoApi consumoApi;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoCertoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Iniciando teste com a SPTrans...");
+		Scanner sc = new Scanner(System.in);
+		var busca = sc.nextLine();
+		String jsonResposta = consumoApi.obterDadosSPTrans("/Linha/Buscar?termosBusca="+ busca);
+
+		System.out.println("Resposta da SPTrans:");
+		System.out.println(jsonResposta);
+
+
+
+	}
+
+
+
 
 }
