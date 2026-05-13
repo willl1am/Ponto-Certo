17525054401be0b43b61c9659092b9e23268af48
William <wllmsantiagogracioso@gmail.com>
2026-05-12 21:13:18 -0300

    Feat: Inicia Backend Spring Boot

---
+++ BackEnd/Ponto-Certo/Ponto-Certo/src/main/java/com/pontocerto/Ponto_Certo/PontoCertoApplication.java
@@ -0,0 +1,13 @@
+package com.pontocerto.Ponto_Certo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoCertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoCertoApplication.class, args);
+	}
+
+}
